14f6e76ddfa2a305ab8489ed8a816d44359a75a3
James Mineo Kobayashi Junior <35144867+JMKobayashi@users.noreply.github.com>
2023-09-25 23:42:25 -0300

    add script that run requests

---
+++ script.py
@@ -0,0 +1,13 @@
+import requests
+from apscheduler.schedulers.background import BlockingScheduler
+TRACK_API_URL = 'localhost:8080/track'
+
+
+def track_request():
+    print(f'Sending request to URL: [{TRACK_API_URL}]')
+    requests.post(url=TRACK_API_URL)
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(track_request, 'interval', minutes=1)
+scheduler.start()
